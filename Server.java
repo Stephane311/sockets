bf00b1d0ed7404781d0f7812d343c403a92a6022
Stephane311 <29291747@student.g.nwu.ac.za>
2019-10-08 12:22:35 +0200

    Socket 5000

--- Server.java
+++ Server.java
@@ -1,14 +1,52 @@
 // The skeleton program for Server
 import java.net.*;
 import java.io.*;
+import java.lang.*;
 
 public class Server
 {
+    //input stream defined
+ private Socket socket = null;
+ private ServerSocket server = null;
+ private DataInputStream in = null;
 
+
+ public Server(int port){
+     try{
+         server = new ServerSocket(port);
+         System.out.println("server started");
+
+         System.out.println("Waiting for a client to connect..");
+
+         socket = server.accept();
+         System.out.println("Client connection accepted");
+
+         in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+         String line ="";
+
+         while(!line.equals("Done")){
+             try {
+                 line = in.readUTF();
+                 System.out.println(line);
+             }
+             catch(IOException i){
+               System.out.println(i);
+             }
+         }
+         socket.close();
+         in.close();
+     }
+
+     catch(IOException ){
+        System.out.println(i);
+     }
+ }
+     
+ 
 }
 
     public static void main(String args[])
     {
         Server server = new Server(5000);
     }
-}
+
